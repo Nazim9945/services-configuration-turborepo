22050509c5240b34dca2f0c6e2d94cf96fafc908
Nazim Saifi <saifinazim2311@gmail.com>
2024-08-25 23:18:50 +0530

    init

---
+++ apps/app-react/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Newone from '@repo/ui/newone'
+
+function App() {
+ 
+
+  return (
+    <>
+    
+        <Newone/>
+        <div className='text-3xl'> tailwind install</div>
+               
+    </>
+  )
+}
+
+export default App
